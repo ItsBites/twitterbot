d1f25b4f360b69bcefb6cba280223b848da16818
ItsBites <32378825+ItsBites@users.noreply.github.com>
2017-10-22 12:54:11 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+// bot features
+// due to the Twitter ToS automation of likes
+// is no longer allowed, so:
+const Twit = require('twit')
+const config = require('./config')
+const consoleLol = require('console.lol')
+
+const bot = new Twit(config.twitterKeys)
+
+const retweet = require('./api/retweet')
+const reply = require('./api/reply')
+
+console.rofl('Bot starting...')
+
+// retweet on keywords
+retweet()
+setInterval(retweet, config.twitterConfig.retweet)
+
+// reply to new follower
+const userStream = bot.stream('user')
+userStream.on('follow', reply)
